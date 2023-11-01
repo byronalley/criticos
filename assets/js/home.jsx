c274df9342879d9011eec716ca6b404458a35faf
PhilJG <phl.j.gray@gmail.com>
2023-10-31 19:31:02 -0700

    add react to root again and add tailwind animation to head

--- assets/js/home.jsx
+++ assets/js/home.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import Reviews from "./Reviews";
-import Featured from "./Feautured";
+import Featured from "./Featured";
 import Footer from "./Footer";
 
 export default function Home() {
