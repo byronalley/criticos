1e9a4f6155af502294ef4bdde3075c237aca0c9f
PhilJG <phl.j.gray@gmail.com>
2023-11-02 14:13:19 -0700

    Transfer navbar to jsx

--- assets/js/home.jsx
+++ assets/js/home.jsx
@@ -3,10 +3,13 @@ import Header from "./Header";
 import Reviews from "./Reviews";
 import Featured from "./Featured";
 import Footer from "./Footer";
+import Navbar from "./Nabar";
 
 export default function Home() {
+  const [user, setUser] = useState(null);
   return (
     <>
+      <Navbar user={user} />
       <Header />
       <Reviews />
       <Footer />
