f6c6085946d47caefab024e7563ce01a4ce8e855
PhilJG <66040850+PhilJG@users.noreply.github.com>
2023-11-17 16:53:31 -0800

    Fetch reviews api (#16)
    * Added fetch to reviews component
    
    ---------
    
    Co-authored-by: Byron Alley <byron@byronalley.com>

--- assets/js/Reviews.jsx
+++ assets/js/Reviews.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Reviews() {
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    fetch("/web_api/reviews")
+      .then((res) => res.json())
+      .then((data) => {
+        setReviews(data.data);
+        console.log(reviews);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <section className="py-12">
       <div className="container mx-auto">
@@ -8,23 +20,28 @@ export default function Reviews() {
           Latest Book Reviews
         </h3>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 mt-6">
-          <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-            <img
-              src="images/white-square.gif"
-              alt="Book Title"
-              className="w-full h-48 object-cover"
-            />
-            <div className="p-6">
-              <h4 className="text-xl font-semibold">Book Title</h4>
-              <p className="text-gray-600 mt-2">Author Name</p>
-              <a
-                href="#"
-                className="inline-block mt-4 bg-blue-500 text-white px-4 py-2 rounded-full hover:bg-blue-600 transition"
-              >
-                Read Review
-              </a>
+          {reviews.map((book) => (
+            <div className="bg-blue-500 rounded-lg shadow-lg overflow-hidden">
+              <img
+                src="images/white-square.gif"
+                alt="Book Title"
+                className="w-full h-48 object-cover"
+              />
+              <div className="p-6">
+                <h4 className="text-xl font-semibold text-white">Book Title</h4>
+                <p className="text-white mt-2 mb-3">Author Name</p>
+                <div className="bg-white px-4 py-2 rounded-md">
+                  <p className="text-black font-semibold">
+                    Rating: {book.rating}
+                  </p>
+
+                  <p className="inline-block mt-4  text-500-blue ">
+                    {book.content}
+                  </p>
+                </div>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </section>
