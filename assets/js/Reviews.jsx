6ca8ee247c67579e6eb56d6142293b1e47068189
PhilJG <phl.j.gray@gmail.com>
2023-11-19 22:28:41 -0800

    Added book api fetch and linked the id's with reviews api

--- assets/js/Reviews.jsx
+++ assets/js/Reviews.jsx
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from "react";
 
 export default function Reviews() {
   const [reviews, setReviews] = useState([]);
+  const [books, setBooks] = useState([]);
 
   useEffect(() => {
-    fetch("/web_api/reviews")
+    fetch("http://localhost:4000/web_api/reviews")
       .then((res) => res.json())
       .then((data) => {
         setReviews(data.data);
@@ -13,6 +14,36 @@ export default function Reviews() {
       .catch((err) => console.log(err));
   }, []);
 
+  useEffect(() => {
+    fetch("http://localhost:4000/web_api/books")
+      .then((res) => res.json())
+      .then((data) => {
+        setBooks(data.data);
+        console.log(books);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  function handleReview() {
+    //map through books
+    books.map((book) => {
+      //initialize reviews as an array
+      book.reviews = [];
+      //map through reviews
+      reviews.map((review) => {
+        if (book.id === review.book_id) {
+          //push the review to the reviews array of the book
+          book.reviews.push(review);
+          console.log(book.reviews);
+        }
+      });
+    });
+  }
+
+  //add reviews properties to books array
+
+  handleReview();
+
   return (
     <section className="py-12">
       <div className="container mx-auto">
@@ -20,7 +51,7 @@ export default function Reviews() {
           Latest Book Reviews
         </h3>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 mt-6">
-          {reviews.map((book) => (
+          {books.map((book) => (
             <div className="bg-blue-500 rounded-lg shadow-lg overflow-hidden">
               <img
                 src="images/white-square.gif"
@@ -28,15 +59,24 @@ export default function Reviews() {
                 className="w-full h-48 object-cover"
               />
               <div className="p-6">
-                <h4 className="text-xl font-semibold text-white">Book Title</h4>
-                <p className="text-white mt-2 mb-3">Author Name</p>
+                <h4 className="text-xl font-semibold text-white">
+                  {book.title}
+                </h4>
+                {/* <p className="text-white mt-2 mb-3">Author Name</p> */}
+                <p className="inline-block mt-4  text-500-blue ">
+                  {book.summary}{" "}
+                </p>
+
                 <div className="bg-white px-4 py-2 rounded-md">
                   <p className="text-black font-semibold">
-                    Rating: {book.rating}
+                    Rating:{" "}
+                    {book.reviews.length > 0
+                      ? book.reviews[0].rating
+                      : "No reviews yet"}
                   </p>
 
                   <p className="inline-block mt-4  text-500-blue ">
-                    {book.content}
+                    {book.reviews[0].content}
                   </p>
                 </div>
               </div>
