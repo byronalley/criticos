5b44d4e102a250cea1b1fc52f890cc07cfae9654
Byron Alley <byron@byronalley.com>
2023-12-12 16:11:39 -0800

    WIP Remove everything but create, delete

--- lib/criticos_web/controllers/web_api/lib/criticos_web/controllers/web_api/user_session_controller.ex
+++ lib/criticos_web/controllers/web_api/lib/criticos_web/controllers/web_api/user_session_controller.ex
@@ -6,30 +6,12 @@ defmodule CriticosWeb.WebAPI.UserSessionController do
 
   action_fallback CriticosWeb.FallbackController
 
-  def index(conn, _params) do
-    users = Accounts.list_users()
-    render(conn, :index, users: users)
-  end
-
   def create(conn, %{"user" => user_params}) do
     with {:ok, %User{} = user} <- Accounts.create_user(user_params) do
       conn
       |> put_status(:created)
       |> put_resp_header("location", ~p"/web_api/users/#{user}")
       |> render(:show, user: user)
-    end
-  end
-
-  def show(conn, %{"id" => id}) do
-    user = Accounts.get_user!(id)
-    render(conn, :show, user: user)
-  end
-
-  def update(conn, %{"id" => id, "user" => user_params}) do
-    user = Accounts.get_user!(id)
-
-    with {:ok, %User{} = user} <- Accounts.update_user(user, user_params) do
-      render(conn, :show, user: user)
     end
   end
 
--- test/criticos_web/controllers/web_api/user_session_controller_test.exs
+++ test/criticos_web/controllers/web_api/user_session_controller_test.exs
@@ -21,13 +21,6 @@ defmodule CriticosWeb.WebAPI.UserSessionControllerTest do
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
   end
 
-  describe "index" do
-    test "lists all users", %{conn: conn} do
-      conn = get(conn, ~p"/web_api/users")
-      assert json_response(conn, 200)["data"] == []
-    end
-  end
-
   describe "create user" do
     test "renders user when data is valid", %{conn: conn} do
       conn = post(conn, ~p"/web_api/users", user: @create_attrs)
@@ -45,29 +38,6 @@ defmodule CriticosWeb.WebAPI.UserSessionControllerTest do
 
     test "renders errors when data is invalid", %{conn: conn} do
       conn = post(conn, ~p"/web_api/users", user: @invalid_attrs)
-      assert json_response(conn, 422)["errors"] != %{}
-    end
-  end
-
-  describe "update user" do
-    setup [:create_user]
-
-    test "renders user when data is valid", %{conn: conn, user: %User{id: id} = user} do
-      conn = put(conn, ~p"/web_api/users/#{user}", user: @update_attrs)
-      assert %{"id" => ^id} = json_response(conn, 200)["data"]
-
-      conn = get(conn, ~p"/web_api/users/#{id}")
-
-      assert %{
-               "id" => ^id,
-               "email" => "some updated email",
-               "password" => "some updated password",
-               "username" => "some updated username"
-             } = json_response(conn, 200)["data"]
-    end
-
-    test "renders errors when data is invalid", %{conn: conn, user: user} do
-      conn = put(conn, ~p"/web_api/users/#{user}", user: @invalid_attrs)
       assert json_response(conn, 422)["errors"] != %{}
     end
   end
