e3fc82d9965375cab1e3635edb9ac5ef1abcb0a3
Phil <phl.j.gray@gmail.com>
2023-12-24 14:17:53 -0800

    Revised login form (not working yet)

--- assets/js/Login.jsx
+++ assets/js/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-export default function Login() {
+export default function Login({ loginUser }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -29,23 +29,28 @@ export default function Login() {
     } catch (error) {
       console.log("An error occurred during login:", error);
     }
-    return (
-      <div>
-        <h1>Login</h1>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button onClick={handleLogin}>Login</button>
-      </div>
-    );
   };
+  return (
+    <div>
+      <h1>Login</h1>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button
+        className="bg-blue-500"
+        onClick={handleLogin}
+      >
+        Login
+      </button>
+    </div>
+  );
 }
