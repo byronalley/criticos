7f3d980e11235a71192022d9740b4b5041fd96d1
Byron Alley <byron@byronalley.com>
2023-12-31 01:14:12 -0800

    Return 401 to current_user when no current user

--- test/criticos_web/controllers/web_api/user_controller_test.exs
+++ test/criticos_web/controllers/web_api/user_controller_test.exs
@@ -6,9 +6,9 @@ defmodule CriticosWeb.WebAPI.UserControllerTest do
   end
 
   describe "current_user" do
-    setup :register_and_log_in_user
+    test "renders user when data is valid", %{conn: conn} do
+      %{conn: conn, user: user} = register_and_log_in_user(%{conn: conn})
 
-    test "renders user when data is valid", %{conn: conn, user: user} do
       conn = get(conn, ~p"/web_api/current_user")
 
       %{email: email, id: id, username: username} = user
@@ -19,10 +19,16 @@ defmodule CriticosWeb.WebAPI.UserControllerTest do
                "username" => ^username
              } = json_response(conn, 200)["data"]
     end
+
+    test "returns 401 when not logged in", %{conn: conn} do
+      conn = get(conn, ~p"/web_api/current_user")
+
+      assert %{} = json_response(conn, 401)
+    end
   end
 
   describe "show user" do
-    setup :register_and_log_in_user
+    setup [:user]
 
     test "renders public data for an existing user", %{conn: conn, user: user} do
       %{id: id, username: username} = user
