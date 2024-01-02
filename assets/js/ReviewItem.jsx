15e703397625898006b7f0dcf11ea1406b3dc942
Phil <phl.j.gray@gmail.com>
2024-01-02 12:23:46 -0800

    Add review and image to each book

--- assets/js/ReviewItem.jsx
+++ assets/js/ReviewItem.jsx
@@ -1,27 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-function ReviewItem({ book }) {
-  return (
-    <div
-      key={book.id}
-      className="bg-blue-500 rounded-lg shadow-lg overflow-hidden"
-    >
-      <img
-        src="images/white-square.gif"
-        alt="Book Title"
-        className="w-full h-48 object-cover"
-      />
-      <div className="p-6">
-        <h4 className="text-xl font-semibold text-white">{book.title}</h4>
-        <p className="text-white mt-2 mb-3">Author Name</p>
-        <div className="bg-white px-4 py-2 rounded-md">
-          <p className="text-black font-semibold">Rating: {book.rating}</p>
-
-          <p className="inline-block mt-4  text-500-blue ">{book.content}</p>
-        </div>
-      </div>
-    </div>
-  );
+function ReviewItem({ books }) {
+  // return (
+  //   <div className="card  xs:mx-4  ">
+  //     <div className="card-inner shadow-lg">
+  //       <div
+  //         className="card-front grid grid-cols-2 gap-2 bg-black p-6
+  //     shadow-[0_0_10px_2px_cyan]
+  //    "
+  //       >
+  //         <img
+  //           src={image[index]?.url}
+  //           alt={image[index]?.filename}
+  //           className=" w-full h-full object-contain "
+  //         />
+  //         <div>
+  //           <p className="text-white font-semibold text-left">
+  //             {book.reviews.length > 0
+  //               ? Array(book.reviews[0].rating).fill("⭐").join("")
+  //               : "No reviews yet"}
+  //           </p>
+  //           <p className="inline-block mt-4 text-white text-left">
+  //             "
+  //             {book.reviews.length > 0
+  //               ? book.reviews[0].content
+  //               : "No reviews yet"}
+  //             "
+  //           </p>
+  //         </div>
+  //       </div>
+  //       <div className="card-back  px-4 py-2 shadow-[0_0_20px_2px_cyan]">
+  //         <h4 className="text-xl font-semibold text-500-blue ">{book.title}</h4>
+  //         <p className="inline-block text-500-blue">{book.summary}</p>
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
 }
 
 export default ReviewItem;
--- assets/js/ReviewList.jsx
+++ assets/js/ReviewList.jsx
@@ -4,15 +4,87 @@ import ReviewItem from "./ReviewItem";
 
 function ReviewsList({ book }) {
   const [reviews, setReviews] = useState([]);
+  const [books, setBooks] = useState([]);
+  const [image, setImage] = useState([]);
 
   useEffect(() => {
-    fetch("/web_api/reviews")
-      .then((res) => res.json())
-      .then((data) => {
-        setReviews(data.data);
-      })
-      .catch((err) => console.log(err));
+    async function fetchData() {
+      try {
+        const resReviews = await fetch("/web_api/reviews");
+        const dataReviews = await resReviews.json();
+        setReviews(dataReviews.data);
+
+        const resBooks = await fetch("http://localhost:4000/web_api/books");
+        const dataBooks = await resBooks.json();
+        setBooks(dataBooks.data);
+
+        const resImages = await fetch("http://localhost:4000/web_api/images");
+        const dataImages = await resImages.json();
+        setImage(dataImages.data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    fetchData();
   }, []);
+
+  useEffect(() => {
+    if (books.length && reviews.length) {
+      const updatedBooks = books.map((book) => {
+        book.reviews = reviews.filter((review) => book.id === review.book_id);
+        return book;
+      });
+      setBooks(updatedBooks);
+    }
+  }, [books, reviews]);
+
+  // useEffect(() => {
+  //   fetch("/web_api/reviews")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setReviews(data.data);
+  //     })
+  //     .catch((err) => console.log(err));
+  // }, []);
+
+  // useEffect(() => {
+  //   fetch("http://localhost:4000/web_api/books")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setBooks(data.data);
+  //     })
+  //     .catch((err) => console.log(err));
+  // }, []);
+
+  // useEffect(() => {
+  //   fetch("http://localhost:4000/web_api/images")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setImage(data.data);
+  //     })
+  //     .catch((err) => console.log(err));
+  // }, []);
+
+  // function handleReview() {
+  //   //map through books
+  //   books.map((book) => {
+  //     //initialize reviews as an array
+  //     book.reviews = [];
+  //     //map through reviews
+  //     reviews.map((review) => {
+  //       if (book.id === review.book_id) {
+  //         //push the review to the reviews array of the book
+  //         book.reviews.push(review);
+  //         console.log(book.reviews);
+  //       }
+  //     });
+  //   });
+  // }
+
+  // //add reviews properties to books array
+
+  // handleReview();
 
   return (
     <section className="py-12">
@@ -21,33 +93,43 @@ function ReviewsList({ book }) {
           Latest Book Reviews
         </h3>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 mt-6">
-          {reviews.map((book) => (
-            // <ReviewItem
-            //   key={book.id}
-            //   book={book}
-            // />
+          {books.map((book, index) => (
             <div
               key={book.id}
-              className="bg-blue-500 rounded-lg shadow-lg overflow-hidden"
+              className="card  xs:mx-4  "
             >
-              <img
-                src="images/white-square.gif"
-                alt="Book Title"
-                className="w-full h-48 object-cover"
-              />
-              <div className="p-6">
-                <h4 className="text-xl font-semibold text-white">
-                  {book.title}
-                </h4>
-                <p className="text-white mt-2 mb-3">Author Name</p>
-                <div className="bg-white px-4 py-2 rounded-md">
-                  <p className="text-black font-semibold">
-                    Rating: {book.rating}
-                  </p>
+              <div className="card-inner shadow-lg">
+                <div
+                  className="card-front grid grid-cols-2 gap-2 bg-gray-700 p-6
+        shadow-[0_0_10px_2px_cyan]"
+                >
+                  <img
+                    src={image[index]?.url}
+                    alt={image[index]?.filename}
+                    className=" w-full h-full object-contain "
+                  />
 
-                  <p className="inline-block mt-4  text-500-blue ">
-                    {book.content}
-                  </p>
+                  <div>
+                    <p className="text-white font-semibold text-left">
+                      {book.reviews && book.reviews.length > 0
+                        ? Array(book.reviews[0].rating).fill("⭐").join("")
+                        : "No reviews yet"}
+                    </p>
+                    <p className="inline-block mt-4 text-white text-left">
+                      "
+                      {book.reviews && book.reviews.length > 0
+                        ? book.reviews[0].content
+                        : "No reviews yet"}
+                      "
+                    </p>
+                  </div>
+                </div>
+                <div className="card-back  px-4 py-2 shadow-[0_0_20px_2px_cyan]">
+                  <h4 className="text-xl font-semibold text-500-blue ">
+                    {book.title}
+                  </h4>
+
+                  <p className="inline-block text-500-blue">{book.summary}</p>
                 </div>
               </div>
             </div>
