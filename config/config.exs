7f06c600209d3c95dd9d5037ddc8c1953d6db798
Byron Alley <byron@byronalley.com>
2024-01-04 17:33:09 -0800

    Update Esbuild config to auto-import React

--- config/config.exs
+++ config/config.exs
@@ -35,7 +35,7 @@ config :esbuild,
   version: "0.17.11",
   default: [
     args:
-      ~w(js/App.jsx --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+      ~w(js/App.jsx --bundle --target=es2017 --outdir=../priv/static/assets --jsx=automatic --external:/fonts/* --external:/images/*),
     cd: Path.expand("../assets", __DIR__),
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
   ]
