5d570eef7aaeffd3cfae99e08bc1eb833826516e
Byron Alley <byron@byronalley.com>
2024-01-04 16:23:29 -0800

    Refactor app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,21 +3,27 @@
 // over to React
 import "phoenix_html";
 
-import React, { useState, useEffect } from "react";
+// TODO(BA): Configure esbuild so we don't need this import
+import React from "react";
+
 import { createRoot } from "react-dom/client";
 
 import Home from "./Home.jsx";
 
 const rootElement = document.getElementById("root");
 
-if (rootElement) {
-  const root = createRoot(rootElement);
-  //root.render(<App />);
-  root.render(
-    <>
+function App() {
+  return (
+    < >
       <Home />
     </>
   );
+}
+
+if (rootElement) {
+  const root = createRoot(rootElement);
+
+  root.render(<App />);
 } else {
   console.error('Could not find element with id "root"');
 }
