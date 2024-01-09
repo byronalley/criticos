5b3b493b798351a8636af8371079f147e49a34c0
Byron Alley <byron@byronalley.com>
2024-01-08 22:36:28 -0800

    Add login/logout logging

--- lib/criticos_web/controllers/web_api/user_session_controller.ex
+++ lib/criticos_web/controllers/web_api/user_session_controller.ex
@@ -4,6 +4,8 @@ defmodule CriticosWeb.WebAPI.UserSessionController do
   alias Criticos.Accounts
   alias CriticosWeb.UserAuth
 
+  require Logger
+
   action_fallback CriticosWeb.FallbackController
 
   def create(conn, %{"user" => user_params}) do
@@ -14,13 +16,23 @@ defmodule CriticosWeb.WebAPI.UserSessionController do
       |> UserAuth.log_in_user_web_api(user, user_params)
       |> render(:show, user: user)
     else
+      Logger.error("Unsuccessful login attempt with email=#{inspect(email)}")
       {:error, :unauthorized}
     end
   end
 
-  def delete(conn, _) do
-    conn
-    |> UserAuth.log_out_user_web_api()
-    |> send_resp(:no_content, "")
+  def delete(%{assigns: %{current_user: user}} = conn, _) do
+    case user do
+      %{id: id, username: username} ->
+        Logger.info("Logout from username=#{username}, id=#{id}")
+
+        conn
+        |> UserAuth.log_out_user_web_api()
+        |> send_resp(:no_content, "")
+
+      _ ->
+        Logger.error("Attempted to logout with no current user")
+        {:error, :unauthorized}
+    end
   end
 end
