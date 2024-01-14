9f5ddac7df569de3b9c2fed08d2704858f643e9c
Phil <phl.j.gray@gmail.com>
2024-01-13 19:53:51 -0800

    Set results to sit on top of later elements

--- assets/js/components/Button.jsx
+++ assets/js/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Button({ children, variant, onClick }) {
   const baseStyle = "font-bold py-2 px-4 rounded lg:w-1/2";
   const primaryStyle =
-    "bg-white hover:bg-blue-500 text-black hover:text-white  w-full mt-4";
+    "bg-white hover:bg-blue-500 text-black hover:text-white  w-full ";
   const secondaryStyle =
     "border-2 border-white bg-transparent hover:bg-white hover:text-gray-700 text-white";
   const noStyle = "bg-transparent hover:bg-transparent text-white";
