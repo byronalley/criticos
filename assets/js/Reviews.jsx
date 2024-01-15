9ef6289bb3e8f3be277f0e6ce53c98e3dda37ada
Phil <phl.j.gray@gmail.com>
2024-01-14 16:14:48 -0800

    Create review component

--- assets/js/Reviews.jsx
+++ assets/js/Reviews.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 
+import Review from "./components/Review";
+
 export default function Reviews() {
   const [reviews, setReviews] = useState([]);
 
@@ -20,29 +22,10 @@ export default function Reviews() {
         </h3>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 mt-6">
           {reviews.map((book, index) => (
-            <div
+            <Review
+              book={book}
               key={index}
-              className="bg-blue-500 rounded-lg shadow-lg overflow-hidden"
-            >
-              <img
-                src="images/white-square.gif"
-                alt="Book Title"
-                className="w-full h-48 object-cover"
-              />
-              <div className="p-6">
-                <h4 className="text-xl font-semibold text-white">Book Title</h4>
-                <p className="text-white mt-2 mb-3">Author Name</p>
-                <div className="bg-white px-4 py-2 rounded-md">
-                  <p className="text-black font-semibold">
-                    Rating: {book.rating}
-                  </p>
-
-                  <p className="inline-block mt-4  text-500-blue ">
-                    {book.content}
-                  </p>
-                </div>
-              </div>
-            </div>
+            />
           ))}
         </div>
       </div>
---
+++ assets/js/components/Review.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Review({ book }) {
+  return (
+    <div className="bg-blue-500 rounded-lg shadow-lg overflow-hidden">
+      <img
+        src="images/white-square.gif"
+        alt="Book Title"
+        className="w-full h-48 object-cover"
+      />
+      <div className="p-6">
+        <h4 className="text-xl font-semibold text-white">Book Title</h4>
+        <p className="text-white mt-2 mb-3">Author Name</p>
+        <div className="bg-white px-4 py-2 rounded-md">
+          <p className="text-black font-semibold">Rating: {book.rating}</p>
+
+          <p className="inline-block mt-4  text-500-blue ">{book.content}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Review;
