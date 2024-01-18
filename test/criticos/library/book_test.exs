7e7b3ffe40c36e280ea5e1c34febc8b782301c19
Byron Alley <byron@byronalley.com>
2024-01-18 09:18:20 -0800

    Require one of title, isbn, google_volume_id

--- test/criticos/library/book_test.exs
+++ test/criticos/library/book_test.exs
@@ -4,24 +4,37 @@ defmodule Criticos.Library.BookTest do
   alias Criticos.Library.Book
   alias Ecto.Changeset
 
+  @valid_params %{
+    title: "title",
+    year: 2011,
+    summary: "short summary",
+    google_volume_id: "someStuff1234",
+    isbn: "9780553898194",
+    image_url: "image_url",
+    author_id: Ecto.UUID.generate(),
+    creator_id: Ecto.UUID.generate()
+  }
+
   describe "changeset/2" do
     test "returns valid changeset from valid arguments" do
-      data = %{
-        title: "title",
-        year: 2011,
-        summary: "short summary",
-        google_volume_id: "someStuff1234",
-        isbn: "9780553898194",
-        image_url: "image_url",
-        author_id: Ecto.UUID.generate(),
-        creator_id: Ecto.UUID.generate()
-      }
+      assert %Changeset{valid?: true} = changeset = Book.changeset(%Book{}, @valid_params)
 
-      assert %Changeset{valid?: true} = changeset = Book.changeset(%Book{}, data)
-
-      for {field, expected} <- data do
+      for {field, expected} <- @valid_params do
         change = Changeset.get_change(changeset, field)
         assert change == expected, "Expected #{field} to be #{expected}, got #{change}"
+      end
+    end
+
+    test "requires at least one of title, google_volume_id, or isbn" do
+      required = ~w[title google_volume_id isbn]a
+      missing_all_required = Map.reject(@valid_params, fn {k, _} -> k in required end)
+
+      refute Book.changeset(%Book{}, missing_all_required).valid?,
+             "A changeset without any required fields should be invalid"
+
+      for field <- required do
+        assert Book.changeset(%Book{}, Map.delete(@valid_params, field)).valid?,
+               "Expected changeset with only #{field} missing to be invalid, got valid"
       end
     end
   end
