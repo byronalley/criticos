2f8615e37b0b38cb8ca112ceb8ca99fdbd42855b
Byron Alley <byron@byronalley.com>
2024-01-26 17:02:07 -0800

    Factor out BookReviewContainer

--- assets/js/Header.jsx
+++ assets/js/Header.jsx
@@ -1,64 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
 
-import BookSearch from "./components/BookSearch";
-import BookResults from "./components/BookResults";
-
-export default function BookReview() {
-  const [results, setResults] = useState([]);
-  const [bookName, setBookName] = useState("");
-  const [author, setAuthor] = useState("");
-  const [bookNameOutput, setBookNameOutput] = useState("");
-  const [authorOutput, setAuthorOutput] = useState("");
-
-  const handleBookInputChange = (event) => {
-    setBookName(event.target.value);
-  };
-
-  const handleAuthorInputChange = (event) => {
-    setAuthor(event.target.value);
-  };
-
-  const handleSearch = async (event) => {
-    event.preventDefault();
-
-    if (bookName !== "" || author !== "") {
-      setBookNameOutput("");
-      setAuthorOutput("");
-
-      try {
-        const response = await fetch(
-          `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(
-            bookName
-          )}+inauthor:${encodeURIComponent(author)}`
-        );
-        const data = await response.json();
-
-        setResults(data.items);
-        console.log(results);
-      } catch (error) {
-        console.error("Error fetching data from Google Books API", error);
-      }
-    } else {
-      handleSearch();
-    }
-  };
-
+export default function Header() {
   return (
-    <header className="h-screen  text-white lg:flex ">
-      <div className=" bg-gray-800 sticky p-12 lg:w-1/2 mx-auto text-center lg:text-left ">
-        <BookSearch
-          bookName={bookName}
-          author={author}
-          handleBookInputChange={handleBookInputChange}
-          handleAuthorInputChange={handleAuthorInputChange}
-          handleSearch={handleSearch}
-        />
-      </div>
-      <div className="bg-gray-400 text-center lg:text-left p-12 lg:w-1/2  h-full">
-        <BookResults
-          results={results}
-          handleSearch={handleSearch}
-        />
+    <header className="bg-gray-800 text-white p-12 ">
+      <div className="container mx-auto text-center">
+        <h2 className="text-4xl font-semibold animate-flip-up animate-ease-out">
+          Discover and Share Your Favorite Books
+        </h2>
+
+        <p className="text-lg mt-4 animate-flip-down animate-delay-500 animate-ease-out">
+          Join our community of book lovers and start sharing your reviews
+          today.
+        </p>
+        <a
+          href="/users/register"
+          className="inline-block mt-6 bg-blue-500 text-white px-6 py-3 rounded-full hover:bg-blue-600 transition"
+        >
+          Get Started
+        </a>
       </div>
     </header>
   );
