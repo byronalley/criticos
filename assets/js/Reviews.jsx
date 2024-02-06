9629a13a551c645bd2627646fe2f55348a24bcaa
Byron Alley <byron@byronalley.com>
2024-02-05 21:49:51 -0800

    Pull in Google books data for live feed

--- assets/js/Reviews.jsx
+++ assets/js/Reviews.jsx
@@ -4,22 +4,58 @@ import Review from "./components/Review";
 
 export default function Reviews() {
   const [reviews, setReviews] = useState([]);
+  const [books, setBooks] = useState([]);
 
   useEffect(() => {
     fetch("/web_api/reviews")
       .then((res) => res.json())
-      .then((data) => {
-        setReviews(data.data);
-      })
-      .catch((err) => console.error(err));
+      .then(({ data: reviewData }) => {
+        setReviews(reviewData);
+
+        const googleVolumeIds = new Set(
+          reviewData
+            .filter(({ google_volume_id }) => google_volume_id)
+            .map((book) => book.google_volume_id),
+        );
+
+        Promise.all(
+          [...googleVolumeIds].map((id) => {
+            if (!id) throw new Error("id was null");
+
+            return fetch(
+              `https://www.googleapis.com/books/v1/volumes/${id}`,
+            ).then((res) => res.json());
+          }),
+        )
+          .then((responses) => {
+            const books = Object.fromEntries(
+              responses.map((book) => [
+                book.id,
+                {
+                  id: book.id,
+                  title: book.volumeInfo.title,
+                  author: book.volumeInfo.authors.join(", "),
+                },
+              ]),
+            );
+
+            setBooks(books);
+          })
+          .catch((err) => console.error(err));
+      });
   }, []);
 
   return (
     <section className="py-12">
       <div className="container mx-auto">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 mt-6">
-          {reviews.map((book, index) => (
-            <Review book={book} key={index} isEditable={false} />
+          {reviews.map((review, index) => (
+            <Review
+              review={review}
+              book={books[review.google_volume_id]}
+              key={index}
+              isEditable={false}
+            />
           ))}
         </div>
       </div>
