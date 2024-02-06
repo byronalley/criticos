5b56c809adbe04ef6c07942076a5bf168a97c455
Byron Alley <byron@byronalley.com>
2024-02-05 21:15:44 -0800

    Remove commented markup

--- assets/js/Reviews.jsx
+++ assets/js/Reviews.jsx
@@ -17,20 +17,9 @@ export default function Reviews() {
   return (
     <section className="py-12">
       <div className="container mx-auto">
-        {/* <h3 className="text-2xl font-semibold txt-gray-800">
-          Latest Book Reviews
-        </h3> */}
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 mt-6">
-          {/* <Review
-            book={"Search"}
-            isEditable={true}
-          /> */}
           {reviews.map((book, index) => (
-            <Review
-              book={book}
-              key={index}
-              isEditable={false}
-            />
+            <Review book={book} key={index} isEditable={false} />
           ))}
         </div>
       </div>
