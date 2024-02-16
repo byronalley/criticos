c35a904b6498c994e0bb4340eeeb54b63e5953d1
Byron Alley <byron@byronalley.com>
2024-02-15 21:27:42 -0800

    Factor out API calls

---
+++ assets/js/lib/BookAPI.js
@@ -0,0 +1,26 @@
+const fetchGoogleVolumes = async (googleVolumeIds) => {
+  const responses = await Promise.all(
+    [...googleVolumeIds].map((id) => {
+      if (!id) throw new Error("id was null");
+
+      return fetch(`https://www.googleapis.com/books/v1/volumes/${id}`).then(
+        (res) => res.json(),
+      );
+    }),
+  );
+
+  const books = Object.fromEntries(
+    responses.map((book) => [
+      book.id,
+      {
+        id: book.id,
+        title: book.volumeInfo.title,
+        author: book.volumeInfo.authors.join(", "),
+      },
+    ]),
+  );
+
+  return books;
+};
+
+export { fetchGoogleVolumes };
