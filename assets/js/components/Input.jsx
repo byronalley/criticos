60111f5e796f2cbf2878ab6e94ade4564e75b1e0
Byron Alley <byron@byronalley.com>
2024-04-07 16:29:08 -0700

    Add input validation for reviews

--- assets/js/components/Input.jsx
+++ assets/js/components/Input.jsx
@@ -1,18 +1,30 @@
 import React from "react";
 
-function Input({ value, onChange, placeholder, isHidden, multiline }) {
+function Input({
+  value,
+  onChange,
+  placeholder,
+  isHidden,
+  multiline,
+  maxLength,
+  id,
+}) {
   return multiline ? (
     <textarea
       className={` w-full  bg-gray-800 text-white px-6 py-3 rounded`}
       type="text"
+      id={id}
       placeholder={placeholder}
       value={value}
       onChange={onChange}
+      maxLength={maxLength}
+      required={true}
     />
   ) : (
     <input
       className={`$ w-full  bg-gray-800 text-white px-6 py-3 rounded`}
       type="text"
+      id={id}
       placeholder={placeholder}
       value={value}
       onChange={onChange}
