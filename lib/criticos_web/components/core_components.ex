e270b71d85b08eae9bf7fb929006d296b7ba533b
Byron Alley <byron@byronalley.com>
2024-08-08 00:14:52 -0700

    Make forms less ugly, more spacey

--- lib/criticos_web/components/core_components.ex
+++ lib/criticos_web/components/core_components.ex
@@ -196,7 +196,7 @@ defmodule CriticosWeb.CoreComponents do
   def simple_form(assigns) do
     ~H"""
     <.form :let={f} for={@for} as={@as} {@rest}>
-      <div class="mt-10 space-y-8 bg-white">
+      <div class="mt-6 px-6 pt-2 pb-8 space-y-8 bg-white rounded-md">
         <%= render_slot(@inner_block, f) %>
         <div :for={action <- @actions} class="mt-2 flex items-center justify-between gap-6">
           <%= render_slot(action, f) %>
